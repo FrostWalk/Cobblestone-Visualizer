71212725952f3c2eb7f443ad9abdbd69f259cd2f
Federico Frigerio <federico.frigerio@studenti.unitn.it>
2024-06-08 12:46:04 +0200

    deps clean

--- src/api/random_seed.rs
+++ src/api/random_seed.rs
@@ -1,18 +1,17 @@
 use actix_web::{get, HttpResponse, Responder};
 use actix_web::http::header::ContentType;
-use derive_new::new;
 use serde::Serialize;
 
 use crate::world_gen_helper::get_random_seed;
 
-#[derive(Serialize, new)]
+#[derive(Serialize)]
 struct SeedResponse {
     seed: u64,
 }
 
 #[get("/randomSeed")]
 pub(crate) async fn generate_seed() -> impl Responder {
-    let response = serde_json::to_string(&SeedResponse::new(get_random_seed())).unwrap();
+    let response = serde_json::to_string(&SeedResponse { seed: get_random_seed() }).unwrap();
 
     HttpResponse::Ok()
         .content_type(ContentType::json())
--- src/robots/sharable_runner.rs
+++ src/robots/sharable_runner.rs
@@ -1,7 +1,5 @@
-use derive_new::new;
 use robotics_lib::runner::Runner;
 
-#[derive(new)]
 pub(crate) struct SharableRunner {
     pub(crate) option_runner: Option<Runner>,
 }
@@ -9,3 +7,11 @@ pub(crate) struct SharableRunner {
 unsafe impl Send for SharableRunner {}
 
 unsafe impl Sync for SharableRunner {}
+
+impl SharableRunner {
+    pub(crate) fn new(r: Option<Runner>) -> Self {
+        Self {
+            option_runner: r
+        }
+    }
+}
